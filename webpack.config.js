9b20aa65ffcbdd36d76027eac9b057e356591369
Ариан <aria-n@mail.ru>
2017-08-31 11:27:54 +0300

    Задание 1.0

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+let webpack = require('webpack');
+let HtmlPlugin = require('html-webpack-plugin');
+let CleanWebpackPlugin = require('clean-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let loaders = require('./webpack.config.loaders')();
+let path = require('path');
+
+loaders.push({
+    test: /\.css$/,
+    loader: ExtractTextPlugin.extract({
+        fallbackLoader: 'style-loader',
+        loader: 'css-loader'
+    })
+});
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve('dist')
+    },
+    devtool: 'source-map',
+    module: {
+        loaders
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                drop_debugger: false
+            }
+        }),
+        new ExtractTextPlugin('styles.css'),
+        new HtmlPlugin({
+            title: 'Loft School sample project',
+            template: 'index.hbs'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ]
+};
